3df1dc8275dcb10df56898f9588a2cf29bec8dc0
Manuel Martín Morante <63638047+Manu200476@users.noreply.github.com>
2021-11-29 07:06:28 +0100

    Clean Amazon request url

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def get_google_suggestions(kw):
     return suggestions
 
 def get_amazon_suggestions(kw):
-    url = f'https://completion.amazon.com/api/2017/suggestions?session-id=132-9553966-7632905&customer-id=A3V141IASC13KS&request-id=0FR95W9VHVCJWA8EJPER&page-type=Gateway&lop=es_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&ks=undefined&prefix={kw}&event=onFocusWithSearchTerm&limit=11&b2b=0&fresh=0&fb=1&suggestion-type=KEYWORD&suggestion-type=WIDGET&_=1637420187493'
+    url = f'https://completion.amazon.com/api/2017/suggestions?lop=es&mid=ATVPDKIKX0DER&alias=aps&prefix={kw}&suggestion-type=KEYWORD&suggestion-type=WIDGET'
     response = requests.get(url)
     data = response.json()
     suggestions = [x['value'] for x in data['suggestions']]
